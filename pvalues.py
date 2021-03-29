4c53c74007f0f01901055b38f3a3f197a3d9fcb0
Jack Bennetto <jack@bennetto.com>
2021-03-29 16:25:45 -0700

    update graphing code and start with alpha-beta plots

--- pvalues.py
+++ pvalues.py
@@ -5,53 +5,51 @@ import numpy as np
 
 
 
-def plot_pvalues(ax, data=None, func=None, dist=None):
+def plot_pvalues(ax, data, show_data=True):
     lower, upper = -0.25, 1.25
     xpts = np.linspace(lower, upper, 500)
-    if dist is not None:
-        ax.plot(xpts,
-                dist.pdf(xpts),
-                color='r',
-                label='actual')
-    if func is None:
-        ax.hist(data,
-                bins=np.linspace(0, 1, 11),
-                density=True)
-    else:
-        kde = func(data)
-        ax.plot(xpts,
-                kde(xpts),
-               label='kde')
-    ax.set_ylim(ymin=0)
-    ax.set_xlim(lower, upper)
-    if data is not None:
+    dist = stats.uniform(0, 1)
+
+    if show_data:
         for i, datum in enumerate(data):
             ax.axvline(datum,
             color='k',
             lw=1,
             alpha=0.2,
             label='data' if i==0 else None)
+    
+    ax.plot(xpts,
+            dist.pdf(xpts),
+            color='r',
+            label='actual')
+
+    kde = reflecting_bounded_kde(data)
+    ax.plot(xpts,
+            kde(xpts),
+            label='kde')
+    ax.set_ylim(ymin=0)
+    ax.set_xlim(lower, upper)
+
+
     ax.legend()
 
+def plot_quantiles(ax, data):
 
-def transforming_bounded_kde(data, minimum=0, maximum=2):
-    """Return a gaussian kde function bounded between two points.
-    Note that it doesn't handle when x is on the edge. 
-    """
-    if (np.any(np.less_equal(data, minimum)) or
-        np.any(np.greater_equal(data, maximum))):
-        raise ValueError("data must be strictly between minimum and maximum")
-    diff = maximum - minimum
-    kde = stats.gaussian_kde(logit((data - minimum)/diff))
-    def transformed_kde(x):
-        x = (x-minimum)/diff
-        return kde(logit(x))/(x*(1-x))/diff
-    return transformed_kde
+    qq = stats.probplot(data,
+                        dist=stats.uniform(0, 1),
+                        fit=False)
+    ax.scatter(qq[0], qq[1])
+    ax.plot([0, 1], [0, 1], 'r')
+    ax.set_xlabel('quantiles of U(0, 1)')
+    ax.set_ylabel('simulated pvalues')
 
-# nb this assume the original kde decays to zero
-# by -1 and 2, so doesn't reflect of both ends
+
 def reflecting_bounded_kde(data):
-    """Return a gaussian kde function bounded between """
+    """Return a gaussian kde function bounded between 0 and 1
+    reflecting gaussians off either end.
+    NB: this assume the original kde decays to zero
+    by -1 and 2, so doesn't reflect of both ends.
+    """
     kde = stats.gaussian_kde(data)
     def rbkde(x):
         result = np.where
